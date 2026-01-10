1d860a31913e7a31c6ab27501d0a59a2de041ccf
Lenzi1902 <144054026+Lenzi1902@users.noreply.github.com>
2026-01-10 15:11:14 +0800

    adding routes api

--- app/Routes/api.php
+++ app/Routes/api.php
@@ -2,9 +2,11 @@
 
 use App\Controllers\AuthController;
 use App\Controllers\PengumumanController;
+use App\Controllers\AbsensiController;
 
 /* Auth */
 $router->post('/api/login', [AuthController::class, 'login']);
+$router->post('/api/absensi', [AbsensiController::class, 'store']);
 
 /* Pengumuman */
 $router->get('/api/pengumuman', [PengumumanController::class, 'index']);
